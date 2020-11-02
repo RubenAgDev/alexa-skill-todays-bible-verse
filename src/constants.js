8ffc3b7d6066bc73b30ff3721c268709039d7c8d
Ruben Aguilar <ruben.ag@Rubens-MacBook-Pro.local>
2020-11-01 22:31:26 -0800

    First commit

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const BIBLE_BASE_URL = "https://text.recoveryversion.bible/";
+const BIBLE_BOOKS = [
+    "Matthew",
+    "Mark",
+    "Luke",
+    "John",
+    "Acts"
+];
+
+
+module.exports = {
+    BIBLE_BASE_URL,
+    BIBLE_BOOKS,
+};
